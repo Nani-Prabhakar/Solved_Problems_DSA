46d15625287c543c2ac3b610a15cfa194bd5f914
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-06-29 18:21:52 +0530

    Add files via upload

---
+++ maximum_product_subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suf=1;int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(pre==0)pre=1;
+            if(suf==0)suf=1;
+            pre=pre*nums[i];
+            suf=suf*nums[nums.size()-i-1];
+            maxi=max(maxi,max(pre,suf));
+        }
+        return maxi;
+    }
+};
