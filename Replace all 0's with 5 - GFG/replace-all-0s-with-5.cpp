f449a5e9e4515dbc6bfd674e6ff22ce93089f21c
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-07-08 00:12:04 +0530

    Added solution - LeetHub

---
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Driver program to test above function
+
+// } Driver Code Ends
+class Solution{
+  public:
+    /*you are required to complete this method*/
+    int convertFive(int n)
+    {
+        string ans=to_string(n);
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]=='0')ans[i]='5';
+        }
+        int res=stoi(ans);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution obj;
+    	cout<<obj.convertFive(n)<<endl;
+    }
+}
+// } Driver Code Ends
