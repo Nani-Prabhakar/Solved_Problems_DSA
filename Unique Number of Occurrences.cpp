43006f5f9376c9bcf9d520444f44a5036d791e9d
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-07-13 00:35:26 +0530

    Add files via upload

---
+++ Unique Number of Occurrences.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+       unordered_map<int,int>freq;
+       for(int i=0;i<n;i++){
+           freq[arr[i]]++;
+       }
+       set<int>s;
+       for(auto i:freq){
+           s.insert(i.second);
+       }
+       return (freq.size()==s.size());
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
