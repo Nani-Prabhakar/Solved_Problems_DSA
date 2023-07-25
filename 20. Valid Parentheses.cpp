2de12f8cbce3fbb29573c9559a4f8ba3ab461754
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-07-25 15:14:35 +0530

    Add files via upload

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')st.push(s[i]);
+            else{
+                if(st.empty())return false;
+                char c=st.top();st.pop();
+                if(s[i]==')'&&c=='('||s[i]==']'&&c=='['||s[i]=='}'&&c=='{'){
+
+                }
+                else return false;
+            }
+        }
+        if(st.empty())return true;
+        return false;
+    }
+};
