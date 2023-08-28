e25dac0b1cbd8eb2ec986733f0776f95c656c0ff
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-08-28 20:09:56 +0530

    Add files via upload

---
+++ implement Stack using queue.cpp
@@ -0,0 +1,44 @@
+//implement Stack using queue::
+
+class MyStack {
+
+public:
+    queue<int>q1;
+    //queue<int>q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+
+        q1.push(x);
+        for(int i=0;i<q1.size()-1;i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
+        //swap(q1,q2);
+    }
+    
+    int pop() {
+        int ans=q1.front();
+        q1.pop();
+        return ans;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
