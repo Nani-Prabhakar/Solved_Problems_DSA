aaf9f51eae4ffa020e9446c809830718b0365b43
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-09-21 21:50:33 +0530

    Add files via upload

--- 81. Search in Rotated Sorted Array 2.cpp
+++ 81. Search in Rotated Sorted Array 2.cpp
@@ -1,10 +1,12 @@
-//33. Search in Rotated Sorted Array
+//81. Search in Rotated Sorted Array II
+
 
 class Solution {
-    int helper(vector<int>arr,int low,int high,int key){
-        if(low>high)return -1;
+    bool helper(vector<int>arr,int low,int high,int key){
+        if(low>high)return 0;
         int mid=(low+high)/2;
-        if(arr[mid]==key)return mid;
+        if(arr[mid]==key)return 1;
+        if(arr[low]==arr[mid] and arr[mid]==arr[high])return helper(arr,low+1,high-1,key);
         if(arr[low]<=arr[mid]){
             if(arr[low]<=key and key<=arr[mid])return helper(arr,low,mid-1,key);
             else return helper(arr,mid+1,high,key);
@@ -13,10 +15,9 @@ class Solution {
             if(arr[mid]<=key and key<=arr[high])return helper(arr,mid+1,high,key);
             else return helper(arr,low,mid-1,key);
         }
-        
     }
 public:
-    int search(vector<int>& nums, int key) {
+    bool search(vector<int>& nums, int key) {
        int low=0;
        int high=nums.size()-1;
        return helper(nums,low,high,key);
