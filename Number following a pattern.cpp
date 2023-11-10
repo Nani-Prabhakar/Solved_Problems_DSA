898c52c83818f627e70d22a4891c0d04b8a2e15f
Nani-Prabhakar <115064749+Nani-Prabhakar@users.noreply.github.com>
2023-11-10 23:06:36 +0530

    Add files via upload

---
+++ Number following a pattern.cpp
@@ -0,0 +1,56 @@
+//Number following a pattern
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string printMinNumberForPattern(string S){
+        // code here 
+        string ans="";
+        int n = S.size();
+        stack<int> st;
+        int cnt=1;
+        for(int i = 0; i <n; i++)
+        {
+            st.push(cnt);
+            cnt++;
+            if(S[i] == 'I')
+            {
+                while(st.size())
+                {
+                    ans+=to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
+        st.push(cnt);
+        cnt++;
+        while(st.size())
+        {
+            ans+=to_string(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.printMinNumberForPattern(S) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
